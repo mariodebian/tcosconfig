661b16dc0c0431d659365ef18e8e494dfc4af035
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2011-02-10 16:56:46 +0100

    global license headers fix

--- TcosGui.py
+++ TcosGui.py
@@ -1,26 +1,22 @@
 # -*- coding: UTF-8 -*-
-##########################################################################
-# tcos_config writen by MarioDebian <mariodebian@gmail.com>
-#
 #    TcosConfig version __VERSION__
 #
-# Copyright (c) 2005 Mario Izquierdo <mariodebian@gmail.com>
+# Copyright (c) 2006-2011 Mario Izquierdo <mariodebian@gmail.com>
 #
-# This program is free software; you can redistribute it and/or modify
+# This package is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
-# any later version.
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+#
+# This package is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-# 02111-1307, USA.
-###########################################################################
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from ConfigReader import ConfigReader
 import shared
